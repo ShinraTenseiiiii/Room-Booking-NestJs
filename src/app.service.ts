f4ef680e1436e568cf5ed890f6ba850ab5a98ace
ShinraTenseiiiii <pandeyjoydeep@gmail.com>
2024-08-22 14:55:50 +0530

    deployed

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): boolean {
-    console.log('hello');
+    console.log('server running on port 3000');
     return true; // boolean responce for flutter
   }
 }
